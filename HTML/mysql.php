349e048e252a937feecd166c93f351b9dba1c625
Nico Fischer <NF@recosnet.int>
2022-03-03 14:56:56 +0100

    added mysql info site

---
+++ HTML/mysql.php
@@ -0,0 +1,20 @@
+<?php
+//These are the defined authentication environment in the db service
+
+// The MySQL service named in the docker-compose.yml.
+$host = $_ENV['MYSQL_HOST'];
+
+// Database use name
+$user = $_ENV['MYSQL_USER'];
+
+//database user password
+$pass = $_ENV['MYSQL_PASSWORD'];
+
+// check the MySQL connection status
+$conn = new mysqli($host, $user, $pass);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} else {
+    echo "Connected to MySQL server successfully!";
+}
+?>
