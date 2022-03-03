2a164328afc76ae6e554a986f1ff2f3b2303d5bb
Nico Fischer <NF@recosnet.int>
2022-03-03 14:52:25 +0100

    now it works!!

--- HTML/info.php
+++ HTML/info.php
@@ -1,20 +1,3 @@
 <?php
-//These are the defined authentication environment in the db service
-
-// The MySQL service named in the docker-compose.yml.
-$host = $_ENV['MYSQL_DATABASE'];
-
-// Database use name
-$user = $_ENV['MYSQL_USER'];
-
-//database user password
-$pass = $_ENV['MYSQL_PASSWORD'];
-
-// check the MySQL connection status
-$conn = new mysqli($host, $user, $pass);
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} else {
-    echo "Connected to MySQL server successfully!";
-}
+phpinfo(15);
 ?>
