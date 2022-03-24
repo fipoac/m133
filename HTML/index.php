09a882e44ee73a8a86ce5e337f4abe2dd26f5725
Nico Fischer <NF@recosnet.int>
2022-03-24 16:39:49 +0100

    changed html to php

---
+++ HTML/index.php
@@ -0,0 +1,246 @@
+<!DOCTYPE html>
+<html lang="de">
+  <head>
+    <title>IT Shop</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=0">
+    <meta name="keywords" content="blog, business, shop, shopping, it, tech, technology, pc, computer-science">
+    <meta name="author" content="Nico Fischer BI19bc">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/slick.css">
+    <link rel="stylesheet" href="assets/css/jquery-ui.css">
+    <link rel="stylesheet" href="assets/css/custom_bootstrap.css">
+    <link rel="stylesheet" href="assets/css/fontawesome.css">
+    <link rel="stylesheet" href="assets/css/elegant.css">
+    <link rel="stylesheet" href="assets/css/plyr.css">
+    <link rel="stylesheet" href="assets/css/aos.css">
+    <link rel="stylesheet" href="assets/css/animate.css">
+    <link rel="stylesheet" href="assets/css/themify-icons.css">
+    <link rel="shortcut icon" href="assets/images/favicon/shortcut_logo.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="assets/images/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="assets/images/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="assets/images/favicon/favicon-16x16.png">
+    <link rel="manifest" href="assets/images/favicon/site.webmanifest">  </head>
+  <body>
+    <div id="main">
+      <header>
+        <div class="header-wrapper">
+          <div class="container">
+            <div class="header-menu">
+              <div class="row no-gutters align-items-center justify-content-center">
+                <div class="col-4 col-md-2"><a class="logo" href="index.html" title="Zur Startseite"><img id="logo-svg" src="assets/images/logo.svg" alt="Logo"></a></div>
+                <div class="col-8 col-md-8">
+                  <div class="mobile-menu"><a href="#" id="showMenu"><i class="fas fa-bars"></i></a></div>
+                  <nav class="navigation">
+                    <ul>
+                      <li class="nav-item"><a class="pisen-nav-link" href="blog.html" title="Blog">Blog</a>
+                      <li class="nav-item"><a class="pisen-nav-link" href="about_us.html" title="Über uns">&Uuml;ber uns</a><i class="submenu-opener fas fa-plus"></i>
+                        <ul class="sub-menu">
+                          <li class="sub-menu_item"><a class="sub-menu-link" href="contact.html" title="Kontakt">Kontakt</a></li>                      
+                        </ul>
+                      </li>
+                      <li class="nav-item"><a class="pisen-nav-link" href="shop.html" title="Shop">Shop</a>
+                    </ul>
+                  </nav>
+                </div>
+                <div class="col-0 col-xl-2">
+                  <div class="menu-function">
+                    <div id="search"><a class="search-btn" href="#" title="Seite durchsuchen"><i class="icon_search"></i></a></div>
+                      <a href="account.html" id="account" title="Benutzerkonto"><i class="icon_profile"></i></a>
+                      <a href="shop_cart.html" id="cart" title="Warenkorb"><i class="icon_bag_alt"></i></a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </header><!--End header-->
+      <section class="posts blog-index">
+        <div class="container">
+          <div class="row justify-content-center">
+            <div class="col-12 col-md-10">
+              <div class="post-block post-classic">
+                <div class="post-img"><img src="assets/images/homepage1/post-1.png" alt="post image"></div>
+                <div class="row">
+                  <div class="col-12 col-md-10 mx-auto">
+                    <div class="post-detail"><a class="post-title" href="blog_detail.html">North Dakota Learns From Norway</a>
+                      <div class="post-credit">
+                        <div class="author"><a class="author-avatar" href="#"><img src="assets/images/avatar/avatar-1.png" alt="auhtor"></a>
+                          <h5 class="author-name">Lucas Norman</h5>
+                        </div>
+                        <h5 class="upload-day">February 17, 2019</h5>
+                        <div class="post-tag"><a href="index.html">tech</a></div>
+                      </div>
+                      <p class="post-describe">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-12 col-md-10">
+              <div class="post-block post-slide">
+                <div class="post-img post-img-slide">
+                  <div class="post-slide_img"><img src="assets/images/homepage1/post-2.png" alt="post image"></div>
+                  <div class="post-slide_img"><img src="assets/images/homepage1/post-1.png" alt="post image"></div>
+                  <div class="post-slide_img"><img src="assets/images/homepage1/post-1.png" alt="post image"></div>
+                </div>
+                <div class="row">
+                  <div class="col-12 col-md-10 mx-auto">
+                    <div class="post-detail"><a class="post-title" href="blog_detail.html">10 States At Risk of Rural Hospital Closures</a>
+                      <div class="post-credit">
+                        <div class="author"><a class="author-avatar" href="#"><img src="assets/images/avatar/avatar-2.png" alt="auhtor"></a>
+                          <h5 class="author-name">Connor Cooper</h5>
+                        </div>
+                        <h5 class="upload-day">February 17, 2019</h5>
+                        <div class="post-tag"><a href="index.html">tech</a></div>
+                      </div>
+                      <p class="post-describe">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-12 col-md-10">
+              <div class="post-block post-slide">
+                <div class="post-img post-img-multi-slide">
+                  <div class="post-slide_img"><img src="assets/images/homepage1/post-3.png" alt="post image"></div>
+                  <div class="post-slide_img"><img src="assets/images/homepage1/post-4.png" alt="post image"></div>
+                  <div class="post-slide_img"><img src="assets/images/homepage1/post-5.png" alt="post image"></div>
+                  <div class="post-slide_img"><img src="assets/images/homepage1/post-4.png" alt="post image"></div>
+                </div>
+                <div class="row">
+                  <div class="col-12 col-md-10 mx-auto">
+                    <div class="post-detail"><a class="post-title" href="blog_detail.html">U.S. Interventions in Latin America, in Photos</a>
+                      <div class="post-credit">
+                        <div class="author"><a class="author-avatar" href="#"><img src="assets/images/avatar/avatar-3.png" alt="auhtor"></a>
+                          <h5 class="author-name">Bill Cunningham</h5>
+                        </div>
+                        <h5 class="upload-day">February 17, 2019</h5>
+                        <div class="post-tag"><a href="index.html">tech</a></div>
+                      </div>
+                      <p class="post-describe">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-12 col-md-10">
+              <div class="post-block post-with-video">
+                <div class="post-video">
+                  <video id="player" poster="assets/images/homepage1/post-6.png" playsinline="" controls="">
+                    <source src="assets/images/audio+video/video.mp4" type="video/mp4">
+                    <track kind="captions" label="English captions" src="" srclang="en" default="">
+                  </video>
+                </div>
+                <div class="row">
+                  <div class="col-12 col-md-10 mx-auto">
+                    <div class="post-detail"><a class="post-title" href="blog_detail.html">2019 Best Cars for the Money</a>
+                      <div class="post-credit">
+                        <div class="author"><a class="author-avatar" href="#"><img src="assets/images/avatar/avatar-4.png" alt="auhtor"></a>
+                          <h5 class="author-name">Russell Yates</h5>
+                        </div>
+                        <h5 class="upload-day">February 17, 2019</h5>
+                        <div class="post-tag"><a href="index.html">tech</a></div>
+                      </div>
+                      <p class="post-describe">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-12 col-md-10">
+              <div class="post-block post-with-audio">
+                <div class="post-audio">
+                  <audio id="audio-player" controls>
+                    <source src="assets/images/audio+video/audio.mp3" type="audio/mpeg">
+                  </audio>
+                </div>
+                <div class="row">
+                  <div class="col-12 col-md-10 mx-auto">
+                    <div class="post-detail"><a class="post-title" href="blog_detail.html">How to View Breast Cancer Survival Rates</a>
+                      <div class="post-credit">
+                        <div class="author"><a class="author-avatar" href="#"><img src="assets/images/avatar/avatar-5.png" alt="auhtor"></a>
+                          <h5 class="author-name">Russell Yates</h5>
+                        </div>
+                        <h5 class="upload-day">February 17, 2019</h5>
+                        <div class="post-tag"><a href="index.html">tech</a></div>
+                      </div>
+                      <p class="post-describe">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-12 col-md-10">
+              <div class="post-block post-with-quote">
+                <div class="post-quote bg-7">
+                  <q>Last year, the market witnessed a real fever" of land, pushing up the land price level to a very high level,</q>
+                  <div class="quote-author">
+                    <h5 class="author-name">Donald Mills</h5>
+                    <h5 class="author-position">Manager</h5>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-12 col-md-10">
+              <div class="post-block post-classic">
+                <div class="post-img"><img src="assets/images/homepage1/post-7.png" alt="post image"></div>
+                <div class="row">
+                  <div class="col-12 col-md-10 mx-auto">
+                    <div class="post-detail"><a class="post-title" href="blog_detail.html">9 Treasury ETFs for a Volatile Market</a>
+                      <div class="post-credit">
+                        <div class="author"><a class="author-avatar" href="#"><img src="assets/images/avatar/avatar-6.png" alt="auhtor"></a>
+                          <h5 class="author-name">Louis May</h5>
+                        </div>
+                        <h5 class="upload-day">February 17, 2019</h5>
+                        <div class="post-tag"><a href="index.html">tech</a></div>
+                      </div>
+                      <p class="post-describe">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-12 text-center">
+              <button class="normal-btn" id="load-more">Load more</button>
+            </div>
+          </div>
+        </div>
+      </section><!--End posts-->
+      <footer>
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-4">
+              <h5 class="footer-link--title">Melden Sie sich f&uuml;r unseren Newsletter an </h5>
+              <form action="" method="post">
+                <div class="email-form">
+                  <input class="input-form" type="text" placeholder="Geben Sie Ihre Email ein">
+                  <button> <i class="fas fa-paper-plane"></i></button>
+                </div>
+                <div class="read-policy">
+                  <input type="checkbox" id="read">
+                  <label for="read">Ich habe die Datenschutzvereinbarung gelesen und akzeptiert</label>
+                </div>
+              </form>
+            </div>
+          </div>
+          <div class="bg-center">
+            <p class="copyright">Schriftwerk ist lizensiert unter</br><a rel="license" href="https://creativecommons.org/licenses/by-sa/4.0/deed.de"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-sa/4.0/88x31.png"/></a></p>
+            <p class="copyright">Rechte an Bildern und anderen Medien sind durch die jeweiligen Rechtebesitzer definiert oder gemeinfrei</p>
+          </div>
+        </div>
+      </footer><!--End footer-->
+      <script src="assets/js/jquery-3.4.0.min.js"></script>
+      <script src="assets/js/jquery-ui.min.js"></script>
+      <script src="assets/js/slick.min.js"></script>
+      <script src="assets/js/plyr.min.js"></script>
+      <script src="assets/js/aos.js"></script>
+      <script src="assets/js/jquery.scrollUp.min.js"></script>
+      <script src="assets/js/masonry.pkgd.min.js"></script>
+      <script src="assets/js/imagesloaded.pkgd.min.js"></script>
+      <script src="assets/js/numscroller-1.0.js"></script>
+      <script src="assets/js/jquery.countdown.min.js"></script>
+      <script src="assets/js/main.js"></script>
+    </div>
+  </body>
+</html>
