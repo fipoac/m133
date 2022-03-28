18a182aaffeacd695374718878f6dbf3b7407026
Nico Fischer <NF@recosnet.int>
2022-03-28 22:12:56 +0200

    first try with foundation

--- html/index.php
+++ html/index.php
@@ -1,3 +1,7 @@
 <?php
-    phpinfo(15)
+  require($_SERVER['DOCUMENT_ROOT'] . '/include/_session.php');
+  // declare vars (maybe temporary)
+  $_SESSION['site_desc'] = "Home";
+
+  include_once($_SERVER['DOCUMENT_ROOT'] . '/views/header.php');
 ?>
---
+++ html/views/header.php
@@ -0,0 +1,51 @@
+<?php
+  // Temporary solution
+  require($_SERVER['DOCUMENT_ROOT'] . '/cfg/_session.php');
+?>
+<!doctype html>
+<html class="no-js" lang="de" dir="ltr">
+    <!-- Site header -->
+    <head>
+        <title>
+            <?php
+                echo $_SESSION['site_desc'] . " - IT Shop";
+            ?>
+        </title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=0">
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <meta name="keywords" content="blog, business, shop, shopping, it, tech, technology, pc, computer-science">
+        <meta name="author" content=<?php echo $_ENV['WEB_AUTHOR'];?>>
+        <link rel="stylesheet" href="css/foundation.css">
+        <link rel="stylesheet" href="css/app.css">
+        <link rel="shortcut icon" href="assets/favicon/shortcut_logo.png">
+        <link rel="apple-touch-icon" sizes="180x180" href="assets/favicon/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="assets/favicon/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="assets/favicon/favicon-16x16.png">
+        <link rel="manifest" href="assets/favicon/site.webmanifest">
+    </head>
+    <body>
+        <header>
+            <div data-sticky-container>
+                <div class="title-bar" data-sticky data-options="marginTop:0" style="width:100%">
+                    <ul class="menu align-center">
+                        <li><a href="shop.php" title="Shop">Shop</a></li>
+                        <li><a href="shop.php" title="Shop">Shop</a></li>
+                        <li><a href="shop.php" title="Shop">Shop</a></li>
+                        <li><a href="shop.php" title="Shop">Shop</a></li>
+                        <li><a href="shop.php" title="Shop">Shop</a></li>
+                        <li><a href="shop.php" title="Shop">Shop</a></li>
+                    </ul>
+                </div>
+            </div>
+        </header>
+
+
+
+
+            <div
+            <ul class="menu align-center">
+                <li><a href=
+            </ul>
+        </nav>
