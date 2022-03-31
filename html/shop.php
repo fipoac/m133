2aa2a480c866dfd17b96c95b5343c02cac209c7b
Nico Fischer <NF@recosnet.int>
2022-03-31 13:03:04 +0200

    reworked navbar

---
+++ html/shop.php
@@ -0,0 +1,11 @@
+<?php
+require($_SERVER['DOCUMENT_ROOT'] . '/cfg/_session.php');
+// declare vars (maybe temporary)
+$_SESSION['site_desc'] = "Home";
+
+include_once($_SERVER['DOCUMENT_ROOT'] . '/views/header.php');
+?>
+<h1>TEST</h1>
+<?php
+include_once($_SERVER['DOCUMENT_ROOT'] . '/views/footer.php');
+?>
--- html/views/header.php
+++ html/views/header.php
@@ -16,12 +16,13 @@ require($_SERVER['DOCUMENT_ROOT'] . '/cfg/_session.php');
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="keywords" content="blog, business, shop, shopping, it, tech, technology, pc, computer-science">
     <meta name="author" content=<?php echo $_ENV['WEB_AUTHOR']; ?>>
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-    <link rel="shortcut icon" href="assets/favicon/shortcut_logo.png">
-    <link rel="apple-touch-icon" sizes="180x180" href="assets/favicon/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="assets/favicon/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="assets/favicon/favicon-16x16.png">
-    <link rel="manifest" href="assets/favicon/site.webmanifest">
+    <link rel="stylesheet" href="/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/css/custom.css">
+    <link rel="shortcut icon" href="/assets/favicon/shortcut_logo.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="/assets/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/assets/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/assets/favicon/favicon-16x16.png">
+    <link rel="manifest" href="/assets/favicon/site.webmanifest">
 </head>
 
 <body>
@@ -29,34 +30,40 @@ require($_SERVER['DOCUMENT_ROOT'] . '/cfg/_session.php');
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <div class="container-fluid">
                 <a class="navbar-brand" href="#">Navbar</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                    aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                         <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="index.php" title="Startseite">Startseite</a>
+                            <a class="nav-link <?php if ($_SERVER['SCRIPT_NAME'] == '/index.php') {echo 'active';} ?>"
+                                aria-current="page" href="index.php" title="Startseite">Startseite</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="about.php" title="&Uuml;ber uns und Impressum">&Uuml;ber uns</a>
+                            <a class="nav-link <?php if ($_SERVER['SCRIPT_NAME'] == '/about.php') {echo 'active';} ?>"
+                                href="/about.php" title="&Uuml;ber uns und Impressum">&Uuml;ber uns</a>
                         </li>
                         <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="shop.php" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false" title="Shop">Shop</a>
-                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <li><a class="dropdown-item" href="shop/shop_cart.php" title="Zum Warenkorb">Warenkorb</a></li>
-                                <li><a class="dropdown-item" href="shop/shop_checkout.php" title="Zur Kasse">Kasse</a></li>
-                                <li>
-                                    <hr class="dropdown-divider">
-                                </li>
-                                <li><a class="dropdown-item" href="#">Something else here</a></li>
-                            </ul>
+                            <a class="nav-link dropdown-toggle <?php if (in_array($_SERVER['SCRIPT_NAME'], ['/shop.php', '/shop/cart.php', '/shop/checkout.php'])) {echo 'active';} ?>"
+                                href="/shop.php" id="navbarDropdown" role="button" data-bs-toggle="dropdown"
+                                aria-expanded="false" title="Shop">Shop</a>
+                            <div class="dropdown-content">
+                                <a class="dropdown-item" href="/shop/cart.php" title="Zum Warenkorb">Warenkorb</a>
+                                <a class="dropdown-item" href="/shop/checkout.php" title="Zur Kasse">Kasse</a>
+                                <hr class="dropdown-divider">
+                                <a class="dropdown-item" href="#">Something else here</a>
+                            </div>
                         </li>
-                    </ul>
-                    <form class="d-flex">
-                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                        <button class="btn btn-outline-success" type="submit">Search</button>
-                    </form>
                 </div>
+                </li>
+                </ul>
+                <form class="d-flex">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success" type="submit">Search</button>
+                </form>
+            </div>
             </div>
         </nav>
     </header>
